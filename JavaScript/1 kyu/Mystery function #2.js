6151aa95583937457671c02d2d2701334a330858
Leon-baton <60987723+Leon-baton@users.noreply.github.com>
2021-12-29 00:13:24 +0300

    Update Mystery function #2.js

--- JavaScript/1 kyu/Mystery function #2.js
+++ JavaScript/1 kyu/Mystery function #2.js
@@ -1,7 +1,14 @@
-var solved =_=> 'x'
-vm.runInNewContext = ()=>{return}
-describe("Fixed Tests",function(){
-  Test.assertEquals(solved('x'),'x',"x");
-
-});
-return
+HASH = 5381
+mask = 5875174
+var solved=function(arg){
+    var hash = HASH
+    var num = arg & arg
+      while (num != 0 && num != -1) {
+        var char = arg & 255
+        num >>= 8
+        hash = ((hash << 5) + hash) + char
+        hash = hash & hash
+      }
+  called = true
+  return hash ^ mask
+}
